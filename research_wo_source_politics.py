b3a8590159892c202e7b7fa34e97f6de87a93914
QingwenZeng123 <qzeng@lion.lmu.edu>
2024-02-19 21:31:11 -0600

    add the context to prompt

--- research_wo_source_politics.py
+++ research_wo_source_politics.py
@@ -3,18 +3,6 @@ import pandas as pd
 from guidance import models, select
 import csv
 
-llama = models.LlamaCpp('orca_mini_v3_7b.Q4_K_M.gguf', 
-n_ctx=4096,  # Context window size
-n_gpu_layers=-1,  # -1 to use all GPU layers, 0 to use only CPU
-verbose=False  # Whether to print debug info
-)
-
-
-# Define the bias evaluation prompt
-bias_evaluation_prompt = (
-    "You are going to be the reader of a political article. Your job is to determine whether the article is biased based on the reader's perspective. You answer either biased or not biased, with no explanation. An article is biased in its presentation of the topic, meaning that it ever exaggerates, misrepresents, omits, or otherwise distorts facts (including by making subjective opinions look like facts) for the purpose of appealing to a certain political group."
-)
-
 
 @guidance
 def qa_bot(lm, query):
@@ -24,24 +12,47 @@ def qa_bot(lm, query):
     return lm
 
 
+def get_llm_answers(data, model_name, context):
+    """
+    This is a method to send the prompt to llm and get answers
+
+    :data: a dataframe used to store prompt and candidats information
+    :model_name: a string to mention what llm used, need to initiate a new model every time ask questions 
+    :context: is a string that provide llm some backgroud information, eg: the definition of bias
+    :return: answers is an array store 'is-biased' or "is-not-biased", and array index is same as article id
+    """
+    answers = [''] * len(data)
+    # Jump over n_ctx article id: [43]
+    for index, row in data.iterrows():
+        # while this article id haven't been evaluated
+        if row['id'] not in [43] and answers[row['id']] == '':
+            print('id is: ', row['id'], 'title is:', row['article_title'])
+            query = context + row['prompt'] # This prompt consist of article title and content
+            if model_name == 'llama':
+                # Load the llama
+                llama = models.LlamaCpp('orca_mini_v3_7b.Q4_K_M.gguf', 
+                                         n_ctx=4096,  # Context window size 
+                                         n_gpu_layers=-1,  # -1 to use all GPU layers, 0 to use only CPU
+                                         verbose=False  # Whether to print debug info
+                                        )
+                lm = llama + qa_bot(query)
+                lm += select(['is-biased', "is-not-biased"], name='answer')
+                print(lm["answer"])
+                answers[row['id']] = str(lm['answer'])
+    print("Predict from", model_name, "without any attributes is :")
+    print(answers)
+    return answers
+
+
+
 # Loading data from excel file
 data = pd.read_csv('data/data_prompt_w_articles_info_only.csv')
-# This is an array to store the nobias/bias of article id = index
-answers = [''] * len(data)
 
-# Jump over n_ctx article id: [43]
-for index, row in data.iterrows():
-    # while this article id haven't been evaluated
-    if row['id'] not in [43] and answers[row['id']] == '':
-        print('index is: ', index, 'id is: ', row['id'])
-        print(row['article_title'])
-        query = row['prompt']
-        lm = llama + qa_bot(query)
-        lm += select(['bias', 'no bias'], name='answer')
-        print(lm["answer"])
-        answers[row['id']] = str(lm['answer'])
-print("Final result is: ")
-print(answers)
+# Background context without any attributes
+context_wo_info = "You are going to be the reader of a political article. Your job is to determine whether the article is biased. You answer either is-biased or is-not-biased, with no explanation. An article is biased in its presentation of the topic, meaning that it ever exaggerates, misrepresents, omits, or otherwise distorts facts (including by making subjective opinions look like facts) for the purpose of appealing to a certain political group."
+
+# call the get_llm_answers to get predict answers
+wo_info_answers = get_llm_answers(data, "llama", context_wo_info)
 
 # Output the result to csv file: result_data/wo_attriute.csv
 filename = "result_data/wo_attribute.cvs"
@@ -52,10 +63,10 @@ with open(filename, mode='w', newline='') as file:
     writer.writerow(['id', 'wo_result'])
 
     # Write the answers to the CSV file
-    for index in range(len(answers)):
-        if answers[index] == '' and index != 43:
+    for index in range(len(wo_info_answers)):
+        if wo_info_answers[index] == '' and index != 43:
             break
-        row = [index, answers[index]]
+        row = [index, wo_info_answers[index]]
         writer.writerow(row)
 
 print(f'Data has been written to {filename}.')
