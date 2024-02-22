9bd707f7d97d94119289f11a7150f9fc9696eecf
ntran18 <108908370+ntran18@users.noreply.github.com>
2024-02-22 12:58:32 -0800

    Adding the analysis result of two experiments: participant politics and article source, also distinguish between article id and the index

--- analysis.py
+++ analysis.py
@@ -2,13 +2,16 @@ import pandas as pd
 
 def collect_analysis(result_file_path, no_other_source = False):
     result_data = pd.read_csv(result_file_path)
-    result_data['result'] = result_data['result'].replace({'bias': 'is-biased', 'no bias': 'is-not-biased'})
-    original_data = pd.read_csv('data/data_articles_info.csv', usecols=['id', 'bias-question', 'politics'])
+    original_data = pd.read_csv('data/data_articles_info.csv', usecols=['id', 'bias-question', 'politics', 'article_id'])
     result_data = result_data[(result_data['id'] != 43) & (result_data['id'] != -1)]
     if no_other_source:
-        merged_data = pd.merge(result_data, original_data, on='id', how='left')
+        merged_data = pd.merge(result_data, original_data, left_on= 'id', right_on='article_id', how='left', validate='one_to_many')
     else:
-        merged_data = pd.concat([result_data, original_data], ignore_index=True)
+        merged_data = pd.merge(result_data, original_data, on='id', how='left', validate='one_to_one')
+        print(merged_data)
+        merged_data = merged_data[merged_data['result'].notna()]
+
+        
     
     # Collect overall data
     correct_predictions = merged_data[merged_data['result'] == merged_data['bias-question']]
@@ -18,20 +21,20 @@ def collect_analysis(result_file_path, no_other_source = False):
     politics_accuracy = {}
     
     for politics_group in merged_data['politics'].unique():
-        # Filter data for the current politics group
-        group_data = merged_data[merged_data['politics'] == politics_group]
+        if not pd.isna(politics_group):
+            # Filter data for the current politics group
+            group_data = merged_data[merged_data['politics'] == politics_group]
 
-        # Compare 'result' and 'bias-question' to calculate accuracy within the politics group
-        correct_predictions = group_data[group_data['result'] == group_data['bias-question']]
-        accuracy = len(correct_predictions) / len(group_data)
+            # Compare 'result' and 'bias-question' to calculate accuracy within the politics group
+            correct_predictions = group_data[group_data['result'] == group_data['bias-question']]
+            accuracy = len(correct_predictions) / len(group_data)
 
-        # Store accuracy in the dictionary with the politics group as the key
-        politics_accuracy[politics_group] = accuracy
+            # Store accuracy in the dictionary with the politics group as the key
+            politics_accuracy[politics_group] = accuracy
     return (overall, politics_accuracy)
 
 
-# result_file_paths = ['result_data/original_prompt_result.csv', 'result_data/participant_politics_result.csv', 'result_data/article_source_result.csv', 'result_data/all_result.csv']
-result_file_paths = ['result_data/original_prompt_result.csv']
+result_file_paths = ['result_data/original_prompt_result.csv', 'result_data/participant_politics_result.csv', 'result_data/article_source_result.csv']
 output_csv_path = 'result_data/analysis_results.csv'
 first_output = []
 conservative = []
@@ -53,6 +56,7 @@ for file in result_file_paths:
     default.append(result[1]['default'])
 
 df = pd.DataFrame({
+    'File Name': result_file_paths,
     'Overall': first_output,
     'Conservative': conservative,
     'Liberal': liberal,
