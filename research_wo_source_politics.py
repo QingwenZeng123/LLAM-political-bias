e68186cf376238a076ee9a39a6a77d1d8fa165a9
CS Admin <ngan170904@gmail.com>
2024-02-29 16:08:20 -0800

    The original result collected was wrong. Modified the guidance query bot from select to generate response

--- research_wo_source_politics.py
+++ research_wo_source_politics.py
@@ -1,6 +1,6 @@
 import guidance
 import pandas as pd
-from guidance import models, select
+from guidance import models, select, gen
 import csv
 
 
@@ -8,15 +8,8 @@ import csv
 def qa_bot(lm, query):
     lm += f'''\
     Q: {query}
-    '''
+    A: {gen(name="answer", stop="Q:")}'''
     return lm
-
- # Load the llama
-llama = models.LlamaCpp('orca_mini_v3_7b.Q4_K_M.gguf', 
-                            n_ctx=4096,  # Context window size 
-                            n_gpu_layers=-1,  # -1 to use all GPU layers, 0 to use only CPU
-                            verbose=False  # Whether to print debug info
-                        )
 
 
 def get_llm_answers(data, model_name, context, prompt_column_name):
@@ -29,19 +22,29 @@ def get_llm_answers(data, model_name, context, prompt_column_name):
     :return: answers is an array store 'is-biased' or "is-not-biased", and array index is same as article id
     """
     answers = [''] * len(data)
+    current_article_id = 0
     # Jump over n_ctx article id: [43]
     for index, row in data.iterrows():
        
         # while this article id haven't been evaluated
-        if prompt_column_name == 'prompt_article_info' and row['article_id'] not in [43] and answers[row['id']] == '':
+        if prompt_column_name == 'prompt_article_info' and row['article_id'] == current_article_id:
+            current_article_id += 1
+            if row['article_id'] == 43:
+                continue
             print('id is: ', row['article_id'])
             query = context + row[prompt_column_name] # This prompt consist of article title and content
             if model_name == 'llama':
+                # Load the llama
+                llama = models.LlamaCpp('orca_mini_v3_7b.Q4_K_M.gguf', 
+                            n_ctx=4096,  # Context window size 
+                            n_gpu_layers=-1,  # -1 to use all GPU layers, 0 to use only CPU
+                            verbose=False  # Whether to print debug info
+                        )
                 lm = llama + qa_bot(query)
-                lm += select(["is-not-biased", "is-biased"], name='answer')
                 print(lm["answer"])
                 answers[row['article_id']] = str(lm['answer'])
         elif prompt_column_name != 'prompt_article_info' and row['article_id'] != 43:
+            print("index is", index)
             query = context + row[prompt_column_name] # This prompt consist of article title and content
             if model_name == 'llama':
                 # Load the llama
@@ -51,7 +54,6 @@ def get_llm_answers(data, model_name, context, prompt_column_name):
                                             verbose=False  # Whether to print debug info
                                         )
                 lm = llama + qa_bot(query)
-                lm += select(['is-biased', "is-not-biased"], name='answer')
                 print(lm["answer"])
                 answers[index] = str(lm['answer'])
     print("Predict from", model_name, "without any attributes is :")
@@ -65,7 +67,10 @@ def run_experiment_and_write_csv(data, model_name, context, output_filename, pro
     # Output the result to a CSV file
     with open(output_filename, mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['id', 'result'])
+        if prompt_column_name == 'prompt_article_info':  
+            writer.writerow(['article_id', 'result'])
+        else:
+            writer.writerow(['id', 'result'])
 
         # Write the answers to the CSV file
         for index, answer in enumerate(answers):
@@ -76,10 +81,9 @@ def run_experiment_and_write_csv(data, model_name, context, output_filename, pro
 
 data = pd.read_csv('data/prompts.csv')
 # Background context without any attributes\t
-context_wo_info = "You are going to be the reader of a political article. Your job is to determine whether the article is biased. You answer either is-biased or is-not-biased, with no explanation. An article is-biased in its presentation of the topic, meaning that it ever exaggerates, misrepresents, omits, or otherwise distorts facts (including by making subjective opinions look like facts) for the purpose of appealing to a certain political group.\n"
+context_wo_info = "You are going to be the reader of a political article. Your job is to determine whether the article is biased based on reader point of view. You answer either is-biased or is-not-biased, with no explanation. An article is-biased in its presentation of the topic, meaning that it ever exaggerates, misrepresents, omits, or otherwise distorts facts (including by making subjective opinions look like facts) for the purpose of appealing to a certain political group.\n"
 
 def run_experiments():
-    #run_experiment_and_write_csv(data, 'llama', context_wo_info, 'result_data/testing.csv', 'nothing')
     #run_experiment_and_write_csv(data, "llama", context_wo_info, "result_data/original_prompt_result2.csv", 'prompt_article_info')
     run_experiment_and_write_csv(data, "llama", context_wo_info, "result_data/participant_politics_result.csv", "prompt_politics_info")
     run_experiment_and_write_csv(data, "llama", context_wo_info, "result_data/article_source_result.csv", "prompt_letter_source_info")
