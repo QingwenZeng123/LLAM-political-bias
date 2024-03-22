f2642b0b5cb4622fdffed6e8eecdddd656f8ace6
QingwenZeng123 <qzeng@lion.lmu.edu>
2024-03-22 14:52:49 -0500

    add hidden env

--- chatgpt_model.py
+++ chatgpt_model.py
@@ -1,16 +1,20 @@
 import openai
+from dotenv import load_dotenv
 import os
-  
-# os.environ["OPENAI_API_KEY"] = ''
 
-client = openai.OpenAI()
+# Load environment variable from .env
+load_dotenv()
+# create a client
+client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
 stream = client.chat.completions.create(
     model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": "Say this is a test"}],
+    messages=[{"role": "user", "content": "Are you a human"}],
     stream=True,
 )
+
 for chunk in stream:
+    # print(chunk.choices[0])
     if chunk.choices[0].delta.content is not None:
         print(chunk.choices[0].delta.content, end="")
